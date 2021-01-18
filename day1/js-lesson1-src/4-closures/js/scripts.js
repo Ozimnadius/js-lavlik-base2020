8d7b8a3275eb855083880b3ba5ae8a787adc33ec
Mikle Seregin <mikhail-seregin@yandex.ru>
2021-01-18 10:07:34 +0300

    first commit

---
+++ day1/js-lesson1-src/4-closures/js/scripts.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function(){
+	let div = document.querySelector('.some');
+	let other = document.querySelector('.other');
+	let btnY = document.querySelector('.addY');
+	let colors = ['#f90', '#f00', '#0f0'];
+	
+	div.addEventListener('click', function(){
+		let num = Math.floor(Math.random() * colors.length);
+		div.style.color = colors[num];
+	});
+	
+	other.addEventListener('click', function(){
+		let num = Math.floor(Math.random() * colors.length);
+		other.style.color = colors[num];
+	});
+
+	btnY.addEventListener('click', function(){
+		colors.push('#ff0');
+		btnY.parentNode.removeChild(btnY);
+	});
+});
+
+/* 
+let x = 1;
+
+function some(){
+	let x = 10;
+
+	function other(){
+		let x = 100;
+		console.log(x);
+	}
+
+	other();
+}
+
+some(); */
